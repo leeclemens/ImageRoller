5a8150712692ddc3be90f96be404a05985c5719c
Lee Clemens <java@leeclemens.net>
2015-11-19 14:01:13 -0500

    Exclude trace logging from coverage (Bug #268)

--- imageroller/__init__.py
+++ imageroller/__init__.py
@@ -45,7 +45,9 @@ class Logger(logging.getLoggerClass()):
         :param args: args passed to _log()
         :param kwargs: kwargs passed to _log()
         """
-        if self.isEnabledFor(Logger.TRACE):
+        # pylint: disable=fixme
+        # TODO: Bug #268: Add logging testing
+        if self.isEnabledFor(Logger.TRACE):  # pragma: no cover
             self._log(Logger.TRACE, msg, args, **kwargs)
 
 
